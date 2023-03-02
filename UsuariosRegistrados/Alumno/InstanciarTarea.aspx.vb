61474038a9b08f565b9a3afc080d7740f26dff54
paabloomaartinez <pablo121002@gmail.com>
2023-03-02 17:31:08 +0100

    intsanciartarea

--- UsuariosRegistrados/Alumno/InstanciarTarea.aspx.vb
+++ UsuariosRegistrados/Alumno/InstanciarTarea.aspx.vb
@@ -1,8 +1,10 @@
 ﻿Public Class InstanciarTarea
     Inherits System.Web.UI.Page
 
+    Dim correo As String
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
 
+        correo = System.Web.HttpContext.Current.Session(“correo”)
     End Sub
 
 End Class
