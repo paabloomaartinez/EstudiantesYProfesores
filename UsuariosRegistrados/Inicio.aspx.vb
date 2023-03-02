e495553a04173ee78bafc62285455ba15379c370
jonortega <jon.ortega.g@gmail.com>
2023-03-02 19:28:10 +0100

    Pagina Login terminada por completo

--- UsuariosRegistrados/Inicio.aspx.vb
+++ UsuariosRegistrados/Inicio.aspx.vb
@@ -6,41 +6,40 @@ Public Class WebForm2
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
         lblContraError.Text() = ""
         lblUserError.Text() = ""
+
+        Dim con = New SqlConnection("Server=tcp:hads2023-jon.database.windows.net,1433;Initial Catalog=HADS2023;Persist Security Info=False;User ID=jortega045@ikasle.ehu.eus@hads2023-jon;Password=Ho44la55;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;")
+        Dim da = New SqlDataAdapter("select * from Usuario", con)
+        Dim ds As New DataSet
+        da.Fill(ds, "Usuario")
+        Dim tbUsuario As New DataTable
+        tbUsuario = ds.Tables("Usuario")
+        Session("tablaUsuarios") = tbUsuario
     End Sub
 
     Protected Sub btnLogin_Click(sender As Object, e As EventArgs) Handles btnLogin.Click
-        AccesoDatos.AccesoDatos.Konektatu()
+        Dim tbUsuarios As DataTable = Session("tablaUsuarios")
 
-        Dim correo As String = tbCorreo.Text()
-        Dim contra As String = tbContrasenia.Text()
+        Dim correo = tbCorreo.Text
+        Dim contrasenia = tbContrasenia.Text
 
-        Dim user As SqlDataReader = AccesoDatos.AccesoDatos.ErabiltzaileakLortu(correo)
-
-        If user.HasRows Then
-            While user.Read()
-                Dim pasahitza As String = user("pass").ToString()
-                Dim egiaztatua As Boolean = user("confirmado")
-                Dim tipo As String = user("tipo").ToString()
-
-                If contra = pasahitza Then
-                    If egiaztatua Then
-                        System.Web.HttpContext.Current.Session(“correo”) = correo
-                        AccesoDatos.AccesoDatos.ItxiKonexioa()
-                        If tipo = "Profesor" Then
-                            Response.Redirect("Profesor.aspx")
-                        ElseIf tipo = "Alumno" Then
-                            Response.Redirect("Alumno.aspx")
-                        End If
+        For Each row As DataRow In tbUsuarios.Rows
+            Dim confirmado As Byte = row("confirmado")
+            Dim email As String = row("email")
+            If (email = correo) And (confirmado) Then
+                Dim pass As String = row("pass")
+                If pass = contrasenia Then
+                    Dim tipo As String = row("tipo")
+                    Session("rol") = tipo
+                    If tipo = "Profesor" Then
+                        Response.Redirect("./Profesor/Profesor.aspx")
                     Else
-                            lblContraError.Text() = "El usuario no esta verificado."
+                        Response.Redirect("./Alumno/Alumno.aspx")
                     End If
-                Else
-                    lblContraError.Text() = "Contraseña incorrecta."
                 End If
-            End While
-        Else
-            lblUserError.Text() = "No existe el correo."
-        End If
-        AccesoDatos.AccesoDatos.ItxiKonexioa()
+            End If
+        Next row
+
+        MsgBox("Email o contraseña incorrectos.")
+
     End Sub
 End Class
