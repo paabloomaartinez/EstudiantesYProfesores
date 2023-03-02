22aad9ada5a310fcde57d2b480448d0aa3f93a9c
paabloomaartinez <pablo121002@gmail.com>
2023-03-02 20:32:11 +0100

    Instanciar Tarea programado pero no probado

--- UsuariosRegistrados/Alumno/InstanciarTarea.aspx.vb
+++ UsuariosRegistrados/Alumno/InstanciarTarea.aspx.vb
@@ -1,10 +1,42 @@
-﻿Public Class InstanciarTarea
+﻿Imports System.Data.SqlClient
+
+Public Class InstanciarTarea
     Inherits System.Web.UI.Page
 
     Dim correo As String
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
 
-        correo = System.Web.HttpContext.Current.Session(“correo”)
+        Dim con As SqlConnection = Session("connection")
+        Dim email As String = Session("correo")
+        Dim da = New SqlDataAdapter("select * from EstudianteTarea where (email) = ('" & email & "')", con)
+        Dim ds As New DataSet
+        da.Fill(ds, "EstudianteTarea")
+        Dim tbEstudianteTarea = ds.Tables("EstudianteTarea")
+        Session("tablaEstudianteT") = tbEstudianteTarea
+        Session("adaptador") = da
+        Session("dataSet") = ds
+
+        txtUsuario.Text() = Session("correo")
+        txtTarea.Text() = Session("tarea")
+        txtEstimado.Text() = Session("hEstimadas")
+        GridView1.DataSource = tbEstudianteTarea
+        GridView1.DataBind()
+
     End Sub
 
+    Protected Sub btnCrear_Click(sender As Object, e As EventArgs) Handles btnCrear.Click
+        Dim tbEstudianteTarea = Session("tablaEstudianteT")
+        Dim R As DataRow = tbEstudianteTarea.NewRow
+        R("correo") = Session("correo")
+        R("codTarea") = Session("tarea")
+        R("hEstimadas") = Session("hEstimadas")
+        R("hReales") = txtReales.Text()
+        tbEstudianteTarea.Rows.Add(R)
+        GridView1.DataSource = tbEstudianteTarea
+        GridView1.DataBind()
+        Dim da = Session("adaptador")
+        Dim ds = Session("dataSet")
+        da.Update(ds, "EstudianteTarea")
+        ds.AcceptChanges()
+    End Sub
 End Class
